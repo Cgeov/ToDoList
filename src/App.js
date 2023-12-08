2de56267af4abfae7feaee5575ae1b6e40cc8194
Christian Tobar <ctobar@premte.com>
2023-12-08 08:17:44 -0600

    [FIX] Corrección de Home Route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/home/home';
 import Task from './pages/task/task';
 
 function App() {
