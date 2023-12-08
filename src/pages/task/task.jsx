f6110a4db037f8d0ed9f1084ee09a6536e6ee6e7
Christian Tobar <ctobar@premte.com>
2023-12-08 08:13:58 -0600

    [UPDATE] Mejoras de Flujos de datos y diseños responsivos

--- src/pages/task/task.jsx
+++ src/pages/task/task.jsx
@@ -2,21 +2,38 @@ import React, { useEffect, useState } from "react";
 import Home from "../home/home";
 import axios from "axios";
 import style from "./task.module.css";
-import MuiAlert from '@mui/material/Alert';
-import { Snackbar } from "@mui/material";
+import MuiAlert from "@mui/material/Alert";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Chip,
+  Skeleton,
+  Snackbar,
+  Typography,
+} from "@mui/material";
+import defaultImg from "../../assets/default.png";
+import { FaEye } from "react-icons/fa";
+import { FaTrashCan } from "react-icons/fa6";
 
 const Alert = React.forwardRef(function Alert(props, ref) {
-    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-  });
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 export default function Task() {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [status, setStatus] = useState("todo");
-  const [dueDate, setDueDate] = useState("");
+  const [dueDate, setDueDate] = useState(new Date().toISOString().slice(0, 10));
   const [data, setData] = useState([]);
   const [hidden, setHidden] = useState(true);
   const [open, setOpen] = React.useState(false);
+  const [openError, setOpenError] = React.useState(false);
+  const [loading, setLoading] = useState(true);
+  const [menssageAlert, setMessageAlert] = useState("");
+  const [indexItem, setIndexItem] = useState(0);
 
   const deleteTask = (id) => {
     axios
@@ -27,60 +44,86 @@ export default function Task() {
         },
       })
       .then(() => {
+        setOpen(true)
+        setMessageAlert('Se ha Elimado la tarea!')
         getData();
       });
   };
 
-  const updateTask = () => {
-    axios
-      .put(`https://apitodo-2yow.onrender.com/items/`, {
-        headers: {
-          "x-api-key":
-            "JzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlRlc3QiLCJpYXQiOjE1MTYyMzkwMjJ9",
-        },
-      })
-      .then(() => {
-        setHidden(!hidden);
-      });
-  };
+  const updateTask = (id) => {
+    var fechaHora = new Date(dueDate);
+    var fechaString = fechaHora.toISOString().split('T')[0];
 
-  const viewTask = (task) => {
-    setName(task.name);
-    setDescription(task.description);
-    setStatus(task.status);
-    setDueDate(task.dueDate);
-    setHidden(false);
-  };
-
-  const createItem = (e) => {
-    e.preventDefault();
-    setOpen(true);
     const toSend = {
       name: name,
       description: description,
       status: status,
-      dueDate: dueDate,
+      dueDate: fechaString, 
     };
+
     axios
-      .post("https://apitodo-2yow.onrender.com/items", toSend, {
+      .put(`https://apitodo-2yow.onrender.com/items/${id}`,toSend, {
         headers: {
           "x-api-key":
             "JzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlRlc3QiLCJpYXQiOjE1MTYyMzkwMjJ9",
         },
       })
-      .then(() => {
-        console.log();
-        
+      .then((response) => {
+        setOpen(true);
+        setMessageAlert('Se ha Modificado la tarea!')
+        setHidden(!hidden);
         getData();
+        resetData();
+      })
+      .catch((error) => {
+        console.log(error)
       });
-    resetData();
+  };
+
+  const viewTask = (task) => {
+    console.log(task)
+    setName(task.name);
+    setDescription(task.description);
+    setStatus(task.status);
+    setDueDate(task.dueDate.split('T')[0]);
+    setHidden(false);
+    setIndexItem(task.id)
+  };
+
+  const createItem = (e) => {
+    e.preventDefault();
+    if(new Date(dueDate) >= new Date()){
+      const toSend = {
+        name: name,
+        description: description,
+        status: status,
+        dueDate: dueDate,
+      };
+      axios
+        .post("https://apitodo-2yow.onrender.com/items", toSend, {
+          headers: {
+            "x-api-key":
+              "JzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlRlc3QiLCJpYXQiOjE1MTYyMzkwMjJ9",
+          },
+        })
+        .then(() => {
+          setMessageAlert('Se ha Creado la tarea!')
+          setOpen(true);
+          getData();
+        });
+      resetData();
+    }else{
+      setOpenError(true)
+    }
+    
   };
 
   const resetData = () => {
     setName("");
     setDescription("");
-    setDueDate(new Date());
+    setDueDate(new Date().toISOString().slice(0, 10));
     setStatus("todo");
+    setIndexItem(0)
   };
 
   useEffect(() => {
@@ -96,147 +139,214 @@ export default function Task() {
         },
       })
       .then((data) => {
-        setData(data.data);
+        console.log(data);
+        setTimeout(function () {
+          setLoading(false);
+          setData(data.data);
+        }, 2000);
       });
   };
 
   const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
 
     setOpen(false);
   };
 
+  const handleCloseError = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpenError(false);
+  };
+
   return (
     <Home>
-        <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
-        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
-          Su tarea fue Creada!
+      <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
+          {menssageAlert}
+        </Alert>
+      </Snackbar>
+      <Snackbar open={openError} autoHideDuration={3000} onClose={handleCloseError}>
+        <Alert onClose={handleCloseError} severity="error" sx={{ width: "100%" }}>
+          La Fecha de finalización debe ser mayor o igual a la actual!
         </Alert>
       </Snackbar>
       <div className={style.containerTaks}>
-      <div>
-        <form onSubmit={createItem}>
-          <div className={style.containerTwo}>
+        <div>
+          <form onSubmit={createItem}>
+            <div className={style.containerTwo}>
+              <div>
+                <label htmlFor="name">Nombre:</label>
+                <input
+                  value={name}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                  required
+                  id="name"
+                  placeholder="Ingrese el nombre de la tarea"></input>
+              </div>
+              <div>
+                <label htmlFor="dueDate">Fecha Finalizacion:</label>
+                <input
+                  value={dueDate}
+                  onChange={(e) => {
+                    setDueDate(e.target.value);
+                  }}
+                  required
+                  type="date"
+                  id="dueDate"
+                  name="dueDate"></input>
+              </div>
+            </div>
+
             <div>
-              <label htmlFor="name">Nombre:</label>
-              <input
-                value={name}
+              <label htmlFor="description">Descripcion:</label>
+              <textarea
+                value={description}
                 onChange={(e) => {
-                  setName(e.target.value);
+                  setDescription(e.target.value);
                 }}
                 required
-                id="name"
-                placeholder="Ingrese el nombre de la tarea"></input>
+                id="description"
+                placeholder="Ingrese la descripcion de la tarea"></textarea>
             </div>
+
             <div>
-              <label htmlFor="dueDate">Fecha Finalizacion:</label>
-              <input
-                value={dueDate}
+              <label>Estado:</label>
+              <select
+                value={status}
                 onChange={(e) => {
-                  setDueDate(e.target.value);
+                  setStatus(e.target.value);
                 }}
-                required
-                type="date"
-                id="dueDate"
-                name="dueDate"></input>
+                name="status">
+                <option value="todo">Por hacer</option>
+                <option value="doing">Realizando</option>
+                <option value="done">Finalizado</option>
+              </select>
             </div>
-          </div>
-
-          <div>
-            <label htmlFor="description">Descripcion:</label>
-            <textarea
-              value={description}
-              onChange={(e) => {
-                setDescription(e.target.value);
+            <button hidden={!hidden} type="submit" value="enviar">
+              Enviar
+            </button>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                console.log(indexItem);
+                updateTask(indexItem);
               }}
-              required
-              id="description"
-              placeholder="Ingrese la descripcion de la tarea"></textarea>
-          </div>
-
-          <div>
-            <label>Estado:</label>
-            <select
-              value={status}
-              onChange={(e) => {
-                setStatus(e.target.value);
-              }}
-              name="status">
-              <option value="todo">Por hacer</option>
-              <option value="doing">Realizando</option>
-              <option value="done">Finalizado</option>
-            </select>
-          </div>
-          <button hidden={!hidden} type="submit" value="enviar">
-            Enviar
-          </button>
-        </form>
-        <div
-          className={style.buttonUpdate}
-          hidden={true}
-          onClick={(e) => {
-            e.preventDefault();
-            updateTask();
-          }}>
-          Actualizar
+              hidden={hidden}
+              value="Guardar">
+              Modificar
+            </button>
+          </form>
         </div>
-      </div>
 
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <td>Nombre</td>
-              <td>Descripcion</td>
-              <td>Estado</td>
-              <td>Fecha Finalizacion</td>
-              <td>Acciones</td>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((element, index) => {
-              return (
-                <tr key={index}>
-                  <td>{element.name}</td>
-                  <td>{element.description}</td>
-                  <td>
-                    <div className={style.chipStatus}>
-                      {element.status === "todo"
-                        ? "Por Hacer"
-                        : element.status === "doing"
-                        ? "En Proceso"
-                        : "Finalizada"}
-                    </div>
-                  </td>
-                  <td>{new Date(element.dueDate).toLocaleDateString()}</td>
-                  <td>
-                    <div className={style.actionsContainer}>
-                      <div
-                        onClick={(e) => {
-                          e.preventDefault();
-                          viewTask(element);
-                        }}
-                        className={style.update}>
-                        Visualizar
-                      </div>
-                      <div
-                        className={style.delete}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          deleteTask(element.id);
-                        }}>
-                        Eliminar
-                      </div>
-                    </div>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+        <div>
+          <h1 style={{ color: "#6c65f2", textAlign: 'center' }}>Tasks</h1>
+          {loading && (
+            <div style={{ width: 205, marginTop: 25 }}>
+              <Skeleton
+                sx={{ height: 140 }}
+                animation="wave"
+                variant="rectangular"
+              />
+              <React.Fragment>
+                <Skeleton
+                  animation="wave"
+                  height={10}
+                  style={{ marginBottom: 6 }}
+                />
+                <Skeleton animation="wave" height={10} width="80%" />
+              </React.Fragment>
+            </div>
+          )}
+          <div className={style.tasks}>
+            {data.map((element, index) => (
+              <Card key={index} sx={{ width: 200 }}>
+                {loading ? (
+                  <Skeleton
+                    sx={{ height: 140 }}
+                    animation="wave"
+                    variant="rectangular"
+                  />
+                ) : (
+                  <CardMedia
+                    sx={{ height: 140, position: "relative" }}
+                    image={defaultImg}
+                    title="green iguana">
+                    <Chip
+                      sx={{
+                        position: "absolute",
+                        right: 5,
+                        top: 5,
+                        color: "#6c65f2",
+                        background: "#ffffffab",
+                      }}
+                      size="small"
+                      label={
+                        element.status === "todo"
+                          ? "Pendiente"
+                          : element.status === "doing"
+                          ? "En Proceso"
+                          : "Finalizada"
+                      }
+                      variant="filled"
+                    />
+                  </CardMedia>
+                )}
+                {loading ? (
+                  <React.Fragment>
+                    <Skeleton
+                      animation="wave"
+                      height={10}
+                      style={{ marginBottom: 6 }}
+                    />
+                    <Skeleton animation="wave" height={10} width="80%" />
+                  </React.Fragment>
+                ) : (
+                  <CardContent>
+                    <Typography
+                      gutterBottom
+                      variant="h5"
+                      component="div"
+                      sx={{ color: "#6c65f2" }}>
+                      {element.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {element.description}
+                    </Typography>
+                  </CardContent>
+                )}
+                <CardActions>
+                  <Button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      viewTask(element);
+                    }}
+                    sx={{ color: "#6c65f2", borderColor: "#6c65f2" }}
+                    size="small"
+                    variant="outlined">
+                    <FaEye size={20} />
+                  </Button>
+                  <Button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      deleteTask(element.id);
+                    }}
+                    sx={{ color: "#6c65f2", borderColor: "#6c65f2" }}
+                    size="small"
+                    variant="outlined">
+                    <FaTrashCan size={20} />
+                  </Button>
+                </CardActions>
+              </Card>
+            ))}
+          </div>
+        </div>
       </div>
     </Home>
   );
